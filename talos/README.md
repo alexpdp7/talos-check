48dc48056b56b3568feae5f6df72cd73c5515618
alex <alex@pdp7.net>
2023-03-25 20:17:53 +0100

    Add crude HTTP endpoint

--- talos/README.md
+++ talos/README.md
@@ -9,3 +9,36 @@
 >>> talos.get_cluster_kubernetes_version()
 'v1.26.1'
 ```
+
+```
+$ poetry run python -m talos.http
+...
+$ curl http://localhost:8000/
+{
+ "available_kubernetes_version": "v1.26.3",
+ "cluster_kubernetes_version": "v1.26.1",
+ "available_talos_version": "v1.3.6",
+ "cluster_talos_versions": [
+  "v1.3.4"
+ ],
+ "needs_kubernetes_update": true,
+ "outdated_talos_versions": [
+  "v1.3.4"
+ ],
+ "needs_talos_update": false,
+ "status": "NEEDS-KUBE-UPDATE-TO-v1.26.3-FROM-v1.26.1"
+}[alex@molly talos-check]$ curl http://localhost:8000/
+{
+ "available_kubernetes_version": "v1.26.3",
+ "cluster_kubernetes_version": "v1.26.1",
+ "available_talos_version": "v1.3.6",
+ "cluster_talos_versions": [
+  "v1.3.4"
+ ],
+ "needs_kubernetes_update": true,
+ "outdated_talos_versions": [
+  "v1.3.4"
+ ],
+ "needs_talos_update": true,
+ "status": "NEEDS-KUBE-UPDATE-TO-v1.26.3-FROM-v1.26.1,NEEDS-TALOS-UPDATE-TO-v1.3.6-FROM-v1.3.4"
+```
---
+++ talos/http.py
@@ -0,0 +1,45 @@
+import http
+import http.server
+import json
+import socketserver
+
+import talos
+
+
+class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        obj = {
+            "available_kubernetes_version": talos.get_available_kubernetes_version(),
+            "cluster_kubernetes_version": talos.get_cluster_kubernetes_version(),
+            "available_talos_version": talos.get_available_talos_version(),
+            "cluster_talos_versions": talos.get_cluster_talos_versions(),
+        }
+        obj["needs_kubernetes_update"] = obj["available_kubernetes_version"] != obj["cluster_kubernetes_version"]
+        obj["outdated_talos_versions"] = [ctv for ctv in obj["cluster_talos_versions"] if ctv != obj["available_talos_version"]]
+        obj["needs_talos_update"] = len(obj["outdated_talos_versions"]) > 0
+
+        statuses = []
+        if obj["needs_kubernetes_update"]:
+            statuses.append(f"NEEDS-KUBE-UPDATE-TO-{obj['available_kubernetes_version']}-FROM-{obj['cluster_kubernetes_version']}")
+        if obj["needs_talos_update"]:
+            outdated_talos_versions = ",".join(set(obj["outdated_talos_versions"]))
+            statuses.append(f"NEEDS-TALOS-UPDATE-TO-{obj['available_talos_version']}-FROM-{outdated_talos_versions}")
+        if not statuses:
+            statuses = "UPDATED"
+        obj["status"] = ",".join(statuses)
+        response = json.dumps(obj, indent=True).encode("utf8")
+        self.send_response(http.HTTPStatus.OK)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Content-length", len(response))
+        self.end_headers()
+        self.wfile.write(response)
+
+
+def main():
+    with socketserver.TCPServer(("", 8000), HTTPRequestHandler) as httpd:
+        print("serving at 8000")
+        httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
