eb09b328a62466d89618d07d2a25eafb60eb059e
alex <alex@pdp7.net>
2023-03-26 13:07:52 +0200

    Make checking available versions optional

--- talos_check/http.py
+++ talos_check/http.py
@@ -2,43 +2,59 @@ import http
 import http.server
 import json
 import socketserver
+import sys
 
 import talos_check
 
 
+def build_cluster_versions(o):
+    o["cluster_kubernetes_version"] = talos_check.get_cluster_kubernetes_version()
+    o["cluster_talos_versions"] = talos_check.get_cluster_talos_versions()
+    return o
+
+
+def build_available_versions(o):
+    o["available_kubernetes_version"] = talos_check.get_available_kubernetes_version()
+    o["available_talos_version"] = talos_check.get_available_talos_version()
+
+    o["needs_kubernetes_update"] = o["available_kubernetes_version"] != o["cluster_kubernetes_version"]
+    o["outdated_talos_versions"] = [ctv for ctv in o["cluster_talos_versions"] if ctv != o["available_talos_version"]]
+    o["needs_talos_update"] = len(o["outdated_talos_versions"]) > 0
+    return o
+
+
 class HTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        obj = {
-            "available_kubernetes_version": talos_check.get_available_kubernetes_version(),
-            "cluster_kubernetes_version": talos_check.get_cluster_kubernetes_version(),
-            "available_talos_version": talos_check.get_available_talos_version(),
-            "cluster_talos_versions": talos_check.get_cluster_talos_versions(),
-        }
-        obj["needs_kubernetes_update"] = obj["available_kubernetes_version"] != obj["cluster_kubernetes_version"]
-        obj["outdated_talos_versions"] = [ctv for ctv in obj["cluster_talos_versions"] if ctv != obj["available_talos_version"]]
-        obj["needs_talos_update"] = len(obj["outdated_talos_versions"]) > 0
+        o = dict()
+        o = build_cluster_versions(o)
+        if self.path == "/available":
+            o = build_available_versions(o)
 
         statuses = []
-        if obj["needs_kubernetes_update"]:
-            statuses.append(f"NEEDS-KUBE-UPDATE-TO-{obj['available_kubernetes_version']}-FROM-{obj['cluster_kubernetes_version']}")
-        if obj["needs_talos_update"]:
-            outdated_talos_versions = ",".join(set(obj["outdated_talos_versions"]))
-            statuses.append(f"NEEDS-TALOS-UPDATE-TO-{obj['available_talos_version']}-FROM-{outdated_talos_versions}")
+        if o.get("needs_kubernetes_update"):
+            statuses.append(f"NEEDS-KUBE-UPDATE-TO-{o['available_kubernetes_version']}-FROM-{o['cluster_kubernetes_version']}")
+        if o.get("needs_talos_update"):
+            outdated_talos_versions = ",".join(set(o["outdated_talos_versions"]))
+            statuses.append(f"NEEDS-TALOS-UPDATE-TO-{o['available_talos_version']}-FROM-{outdated_talos_versions}")
         if not statuses:
-            statuses = "UPDATED"
-        obj["status"] = ",".join(statuses)
-        response = json.dumps(obj, indent=True).encode("utf8")
+            statuses = ["OK"]
+        o["status"] = ",".join(statuses)
+        response = json.dumps(o, indent=True).encode("utf8")
         self.send_response(http.HTTPStatus.OK)
         self.send_header("Content-type", "application/json")
-        self.send_header("Content-length", len(response))
+        self.send_header("Content-Length", len(response))
         self.end_headers()
         self.wfile.write(response)
+        self.wfile.write(b"\n")
 
 
 def main():
     with socketserver.TCPServer(("", 8000), HTTPRequestHandler) as httpd:
         print("serving at 8000")
-        httpd.serve_forever()
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            sys.exit(0)
 
 
 if __name__ == "__main__":
