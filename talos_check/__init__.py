4eeaf00ee2c220198ef144db4932d4bdd9d046e7
alex <alex@pdp7.net>
2025-05-06 22:16:26 +0200

    Fix for real this time

--- talos_check/__init__.py
+++ talos_check/__init__.py
@@ -64,7 +64,7 @@ def get_latest_available_supported_kubernetes_version():
         if release in supported_kubernetes_versions:
             previous_patches = schedule["previousPatches"]
             if len(previous_patches) > 0:
-                release_latest_patch = previous_patches["release"]
+                release_latest_patch = previous_patches[0]["release"]
                 supported_latest[release] = release_latest_patch
     return supported_latest
 
